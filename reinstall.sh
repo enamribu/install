e9afb36d53dc06b6fe5bb82342acf921b3c0f425
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-10-26 14:25:20 +0700

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3330,6 +3330,18 @@ if is_secure_boot_enabled; then
     error_and_exit "Please disable secure boot first."
 fi
 
+# 密码
+if ! is_netboot_xyz && [ -z "$password" ]; then
+    if is_use_dd; then
+        echo "
+This password is only used for SSH access to view logs during the DD process.
+Password of the image will NOT modify.
+"
+
+    fi
+    prompt_password
+fi
+
 # 必备组件
 install_pkg curl grep
 

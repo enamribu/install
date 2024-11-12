c9aedb594f8d7a064e77a774af06db7153d3fa07
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2024-11-12 17:39:37 +0700

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -272,15 +272,17 @@ test_url_real() {
     # ${PIPESTATUS[n]} 表示第n个管道的返回值
     echo $url
     for i in $(seq 5 -1 0); do
-        if wget --timeout=10 --tries=1 --quiet --output-document="$tmp_file" "$url"; then
+        if command curl --insecure --connect-timeout 10 -Lfr 0-1048575 "$url" \
+            1> >(exec head -c 1048576 >$tmp_file) \
+            2> >(exec grep -v 'curl: (23)' >&2); then
             break
         else
             ret=$?
             msg="$url not accessible"
             case $ret in
-            4) failed "$msg" ;;                # HTTP 4xx errors (client errors)
-            8) failed "$msg" ;;                # HTTP 5xx errors (server errors)
-            *) [ $i -eq 0 ] && failed "$msg" ;; # Other errors
+            22) failed "$msg" ;;                # 403 404
+            23) break ;;                        # 限制了空间
+            *) [ $i -eq 0 ] && failed "$msg" ;; # 其他错误
             esac
             sleep 1
         fi
